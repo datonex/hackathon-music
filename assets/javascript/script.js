8200ad7075c02cae1b8035d882175d1994266ce9
cafalchio <maolivei@tcd.ie>
2022-11-17 20:51:19 +0000

    added function definition to scripts

---
+++ assets/javascript/script.js
@@ -0,0 +1,33 @@
+// create sequences object
+const songs = {
+    "song1": [],
+    "song2" : [],
+}
+
+
+// functions to add click to squares
+function addClickEvent() {
+    // get all squares and add click to squares
+}
+
+// function to play songs in sequence
+// calls play audio
+function playSongs(songs) {
+}
+
+// function to play a single audio
+function playAudio(audio) {
+}
+
+// function to start the game
+function startGame() {
+}
+
+// create endgame screen and show points
+function endGame() {
+}
+
+// Game loop function
+function gameLoop() {
+    // keep track of points, username, calls songs
+}
